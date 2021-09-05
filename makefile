ffda1ceff5d1b9f48d2dfa6fc1c059c4b2f47853
Ernest <iendeavor.github.io@gmail.com>
2021-09-05 21:42:51 +0800

    chore: add scripts

--- makefile
+++ makefile
@@ -4,5 +4,13 @@ lint:
 test:
 	yarn jest --coverage ./packages
 
+install-all:
+	yarn install
+	node scripts/walk-examples yarn install
+
 test-all:
 	yarn jest --coverage
+
+clean-all:
+	node scripts/walk-examples rm -rf node_modules
+	rm -rf node_modules
---
+++ scripts/walk-examples.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+const childProcess = require("child_process");
+
+const ignoredPaths = [/node_modules/, /dist/];
+
+main();
+
+function main() {
+  const args = process.argv.slice(2);
+  const command = args.join(" ");
+  console.assert(command.trim() !== "");
+
+  const rootDir = path.resolve(__dirname, "..", "examples");
+  const exampleDirectories = collectExampleDirectories(rootDir);
+  exampleDirectories.forEach((dir) => execCommand(dir, command));
+}
+
+function collectExampleDirectories(dir, exampleDirectories = []) {
+  for (const p of fs.readdirSync(dir)) {
+    if (ignoredPaths.some((ignoredPath) => ignoredPath.test(p))) continue;
+
+    const absP = path.resolve(dir, p);
+    if (p === "package.json") {
+      exampleDirectories.push(dir);
+    }
+
+    if (fs.lstatSync(absP).isDirectory()) {
+      collectExampleDirectories(absP, exampleDirectories);
+    }
+  }
+
+  return exampleDirectories;
+}
+
+function execCommand(dir, command) {
+  console.log("=".repeat(80));
+  console.log(`> cd ${dir} && ${command}`);
+  console.log("=".repeat(80));
+  childProcess.execSync(`cd ${dir} && ${command}`, {
+    stdio: "inherit",
+  });
+}
